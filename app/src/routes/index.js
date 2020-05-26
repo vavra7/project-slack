b57434d26c403abdb78d5b193ebe1e28a1818ad0
Tomáš Vavřinka <email@tomasvavrinka.cz>
2020-05-26 07:37:11 +0200

    styling

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -5,6 +5,7 @@ import Register from './Register';
 import Login from './Login';
 import CreateTeam from './CreateTeam';
 import decode from 'jwt-decode';
+import ViewTeam from './ViewTeam';
 
 const isAuthenticated = () => {
   const token = localStorage.getItem('token');
@@ -43,6 +44,7 @@ export default () => (
       <Route path="/" exact component={Home} />
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={Login} />
+      <Route path="/view-team" exact component={ViewTeam} />
       <PrivateRoute path="/create-team" exact component={CreateTeam} />
     </Switch>
   </BrowserRouter>
